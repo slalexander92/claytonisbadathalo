e46f08297dc2d25874dfeb71765b01cd7e5e16d3
Spencer Alexander <slalexander92@gmail.com>
2021-01-01 14:26:56 -0800

    chore: upgrade site to vue

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <img class="clayton-image" src="./assets/clayton-sucks-at-halo.gif">
+    <h1>:)</h1>
+    <img class="clayton-image" src="./assets/clayton.jpg">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+#app {
+  background-color: purple;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh	;
+}
+</style>
